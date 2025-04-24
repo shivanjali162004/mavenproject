0de2e35fa354556515e814380b05567f17450a3c
Your Name <you@example.com>
2025-04-24 13:48:11 +0530

    first commit

---
+++ src/main/java/com/pg4/www/prgrm4/App.java
@@ -0,0 +1,13 @@
+package com.pg4.www.prgrm4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
